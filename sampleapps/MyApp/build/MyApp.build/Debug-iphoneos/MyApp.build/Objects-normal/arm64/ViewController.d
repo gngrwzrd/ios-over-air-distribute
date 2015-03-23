9a1eb66e5b2c58d9e4a020c9bf75c64afcc61269
Aaron Smith <gngrwzrd@gmail.com>
2015-03-22 17:41:50 -0700

    removed build files

--- sampleapps/MyApp/build/MyApp.build/Debug-iphoneos/MyApp.build/Objects-normal/arm64/ViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyApp/MyApp/ViewController.m \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyApp/MyApp/ViewController.h
--- sampleapps/MyApp/build/MyApp.build/Debug-iphonesimulator/MyApp.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyApp/MyApp/main.m \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyApp/MyApp/AppDelegate.h
