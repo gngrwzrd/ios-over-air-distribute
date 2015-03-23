91d850dc4e8fbdbe48d28e694d2191f2cf4d4f6f
Aaron Smith <gngrwzrd@gmail.com>
2015-03-22 17:41:35 -0700

    Removed build files

--- sampleapps/MyEntApp/build/MyEntApp.build/Debug-iphonesimulator/MyEntApp.build/Objects-normal/x86_64/ViewController.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyEntApp/MyEntApp/ViewController.m \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyEntApp/MyEntApp/ViewController.h
--- sampleapps/MyEntApp/build/MyEntApp.build/Debug-iphonesimulator/MyEntApp.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyEntApp/MyEntApp/main.m \
-  /Users/aaronsmith/Development/Aaron/ios-over-air-distribute/sampleapps/MyEntApp/MyEntApp/AppDelegate.h
